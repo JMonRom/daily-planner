092daad9fdb26d6d5c9f90a9307ee61217daebec
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-07-24 22:03:57 -0700

    Got color to change according to time

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 var plannerContent = [];
 // sets current day on site
 var currentUserDay = moment().format("MMM Do, YYYY");
-$('#currentDay').text(currentUserDay)
-
-// $('button').on('click', function() {
-//   preventDefault();
-
-//   var container = $(this).parent();
-//   var plannerText = container.find('input').val();
-//   var plannerInput = container.find('input').val();
-
-//   var textInput = {
-//     'planner-Text': plannerText,
-//     'planner-Info': plannerInput
-//   };
-
-//   if (textInput['planner-Text'] !== '') {
-//     plannerContent.push(textInput);
-//     localStorage.setItem('planner-Events', JSON.stringify(plannerContent))
-//   }
-// })
+$('#currentDay').text(currentUserDay);
 
 $(document).ready(function() {
+
+  $('.saveBtn').on('click', function() {
+    let scheduleText = $(this).sibling('.text-Info').val();
+    let scheduleTime = $(this).parent().attr('id');
+
+    localStorage.setItem(scheduleTime, scheduleText);
+  })
 
   function indicateTimeColor() {
   let currentTime = moment().hour();
@@ -44,16 +33,11 @@ $(document).ready(function() {
     else {
       $(this).removeClass('present')
       $(this).removeClass('past')
-      $(this).addClass('future')
-    }}
+      $(this).addClass('future') }
+    }
   )}; 
 
-  $('.saveBtn').on('click', function(){
-    let scheduleText = $(this).sibling('.text-Info').val();
-    let scheduleTime = $(this).parent().attr('id');
-
-    localStorage.setItem(sheduleTime, scheduleText);
-  })
+  // $('#9 .text-Info').val(localStorage.getItem('9'))
 
   indicateTimeColor();
 })
