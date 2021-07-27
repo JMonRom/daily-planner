cd8b2a9bd2881913bdfcc431fc90c0eea6aae886
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-07-26 18:55:43 -0700

    Worked on getting highlighted part to work

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $('#currentDay').text(currentUserDay);
 $(document).ready(function() {
 
   $('.saveBtn').on('click', function() {
-    let scheduleText = $(this).sibling('.text-Info').val();
+    let scheduleText = $(this).siblings('.description').val();
     let scheduleTime = $(this).parent().attr('id');
 
     localStorage.setItem(scheduleTime, scheduleText);
@@ -15,12 +15,12 @@ $(document).ready(function() {
   function indicateTimeColor() {
   let currentTime = moment().hour();
   
-  $('.time-Slots').each(function () {
+  $('.time-block').each(function () {
     let timeColorChange = parseInt($(this).attr('id').split('hour')[1]);
 
     if (timeColorChange < currentTime){
-      $(this).removeClass('present')
       $(this).removeClass('future')
+      $(this).removeClass('present')
       $(this).addClass('past')
     }
 
@@ -37,7 +37,15 @@ $(document).ready(function() {
     }
   )}; 
 
-  // $('#9 .text-Info').val(localStorage.getItem('9'))
+  $('#nine .description').val(localStorage.getItem('nine'))
+  $('#ten .description').val(localStorage.getItem('ten'))
+  $('#eleven ..description').val(localStorage.getItem('eleven'))
+  $('#twelve ..description').val(localStorage.getItem('twelve'))
+  $('#one ..description').val(localStorage.getItem('one'))
+  $('#two ..description').val(localStorage.getItem('two'))
+  $('#three ..description').val(localStorage.getItem('three'))
+  $('#four ..description').val(localStorage.getItem('four'))
+  $('#five ..description').val(localStorage.getItem('five'))
 
   indicateTimeColor();
 })
